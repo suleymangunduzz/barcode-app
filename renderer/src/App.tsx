20506fa06cb93afa186018e7e2bc47f715f2e1e5
Suleyman GUNDUZ <suleymangunduzz@gmail.com>
2026-01-07 21:23:34 +0100

    feat: add localization with i18n

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 function App() {
   useEffect(() => {
@@ -11,12 +12,17 @@ function App() {
     });
   }, []);
 
+  const { t, i18n } = useTranslation();
+
+  console.log("Current language:", i18n.language);
+
   return (
     <div className="h-[300px] flex items-center justify-center bg-slate-900">
       <h1 className="text-4xl font-bold text-red-100">Barcode System</h1>
       <div className="h-screen bg-slate-900 flex items-center justify-center">
         <h1 className="text-white text-4xl font-bold">Tailwind is WORKING</h1>
         <div className="bg-red-500">test</div>
+        <h1>{t("text")}</h1>
       </div>
     </div>
   );
---
+++ renderer/src/i18n.ts
@@ -0,0 +1,23 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import enTranslations from "./translations/en.json";
+import trTranslations from "./translations/tr.json";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: enTranslations,
+    },
+    tr: {
+      translation: trTranslations,
+    },
+  },
+  lng: "tr",
+  fallbackLng: "tr",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
